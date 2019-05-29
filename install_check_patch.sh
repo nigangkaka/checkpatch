c28eec23e32f5496a1239e8e5b09a0cef2c6507a
Kaka Ni <nigang@huaqin.corp-partner.google.com>
2019-05-29 15:50:42 +0800

    Update git-checkpath

---
+++ install_check_patch.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#
+# install_check_patch.sh
+#
+# Use this script to fetch the latest checkpatch.pl from GitHub
+
+TEMP="/tmp/install_check_patch/"
+
+readonly CHECKPATCH_SCRIPT="checkpatch.pl"
+readonly SPELLING_TXT="spelling.txt"
+readonly CONST_STRUCTS="const_structs.checkpatch"
+
+readonly KERNEL_RAW_URL="https://raw.githubusercontent.com/torvalds/linux/master"
+readonly CHECKPATCH_URL="${KERNEL_RAW_URL}/scripts/${CHECKPATCH_SCRIPT}"
+readonly SPELLING_URL="${KERNEL_RAW_URL}/scripts/${SPELLING_TXT}"
+readonly CONST_STRUCTS_URL="${KERNEL_RAW_URL}/scripts/${CONST_STRUCTS}"
+
+readonly GIT_CHECKPATCH="git-checkpatch"
+readonly GIT_CHECKPATCH_URL="https://raw.githubusercontent.com/nigangkaka/checkpatch/master/git-checkpatch"
+
+rm -rf ${TEMP}
+mkdir ${TEMP}
+pushd ${TEMP}
+
+for download in "${CHECKPATCH_URL}:${CHECKPATCH_SCRIPT}" "${SPELLING_URL}:${SPELLING_TXT}" "${GIT_CHECKPATCH_URL}:${GIT_CHECKPATCH}" "${CONST_STRUCTS_URL}:${CONST_STRUCTS}"; do
+	echo "Downloading '${download##*:}'..."
+	curl -f "${download%:*}" -s -S -O || \
+		exit 1
+	chmod 755 ${download##*:}
+	sudo cp ${download##*:} /usr/local/bin/
+done
+
+popd
+rm -rf ${TEMP}
