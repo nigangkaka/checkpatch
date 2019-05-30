48ebd64b271a6a93afd57b954a13065f75e8c841
Kaka Ni <nigang@huaqin.corp-partner.google.com>
2019-05-30 14:44:56 +0800

    Fix a bug that the temp out dir not cleared

--- install_check_patch.sh
+++ install_check_patch.sh
@@ -9,11 +9,13 @@ TEMP="/tmp/install_check_patch/"
 readonly CHECKPATCH_SCRIPT="checkpatch.pl"
 readonly SPELLING_TXT="spelling.txt"
 readonly CONST_STRUCTS="const_structs.checkpatch"
+readonly LINDENT="Lindent"
 
 readonly KERNEL_RAW_URL="https://raw.githubusercontent.com/torvalds/linux/master"
 readonly CHECKPATCH_URL="${KERNEL_RAW_URL}/scripts/${CHECKPATCH_SCRIPT}"
 readonly SPELLING_URL="${KERNEL_RAW_URL}/scripts/${SPELLING_TXT}"
 readonly CONST_STRUCTS_URL="${KERNEL_RAW_URL}/scripts/${CONST_STRUCTS}"
+readonly LINDENT_URL="${KERNEL_RAW_URL}/scripts/${LINDENT}"
 
 readonly GIT_CHECKPATCH="git-checkpatch"
 readonly GIT_CHECKPATCH_URL="https://raw.githubusercontent.com/nigangkaka/checkpatch/master/git-checkpatch"
@@ -22,7 +24,11 @@ rm -rf ${TEMP}
 mkdir ${TEMP}
 pushd ${TEMP}
 
-for download in "${CHECKPATCH_URL}:${CHECKPATCH_SCRIPT}" "${SPELLING_URL}:${SPELLING_TXT}" "${GIT_CHECKPATCH_URL}:${GIT_CHECKPATCH}" "${CONST_STRUCTS_URL}:${CONST_STRUCTS}"; do
+for download in "${CHECKPATCH_URL}:${CHECKPATCH_SCRIPT}"\
+		"${SPELLING_URL}:${SPELLING_TXT}"\
+		"${GIT_CHECKPATCH_URL}:${GIT_CHECKPATCH}"\
+		"${CONST_STRUCTS_URL}:${CONST_STRUCTS}"\
+		"${LINDENT_URL}:${LINDENT}"; do
 	echo "Downloading '${download##*:}'..."
 	curl -f "${download%:*}" -s -S -O || \
 		exit 1
